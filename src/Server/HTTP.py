ad67740e7d83708ae29323c3c432f2945dba6f4f
Jarrah-95 <jarrah-95@live.com.au>
2016-03-22 10:57:10 +1100

    Implement HTTPS server with SSL.

--- src/Server/HTTP.py
+++ src/Server/HTTP.py
@@ -1,22 +1,54 @@
 import sys
 from HTTP.server import HTTPServer
 from HTTP.server import SimpleHTTPRequestHandler
+import HTTP.cookiejar
+import HTTP.cookies
+import socket
+import ssl
+
+class SecureHTTPServer(HTTPServer):
+
+    def __init__(self, server_address, HandlerClass, RequestHandlerClass):
+        BaseServer.__init__(self, server_address, HandlerClass)
+        super().__init__(server_address, RequestHandlerClass)
+        HTTPServer.socket = ssl.wrap_socket(HTTPServer.socket)  # TODO: Finish this. 
+
+        self.server_bind()
+        self.server_activate()
 
 
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass = HTTPServer
+class SecureHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def setup(self):
+        self.connection = self.request
+        # Convert to python 3
 
-Protocol = "HTTPS/1.1"
 
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 443
-server_address = ('', port)
+def test(HandlerClass = SecureHTTPRequestHandler,
+         ServerClass = SecureHTTPServer):
+    server_address = ('', 443)
+    httpd = ServerClass(server_address, HandlerClass)
+    sa = httpd.socket.getsocname()
+    print("serving HTTPS on %s port %s", sa[0], sa[1])
 
-HandlerClass.protocol_version = Protocol
-httpd = ServerClass(server_address, HandlerClass)
+    httpd.serve_forever()
 
-sa = httpd.socket.getsockname()
-print("Serving HTTP on", sa[0], "port", sa[1], "...")
-httpd.serve_forever()
+if __name__ == '__main__':
+    test()
+#
+# HandlerClass = SimpleHTTPRequestHandler
+# ServerClass = HTTPServer
+#
+# Protocol = "HTTPS/1.1"
+#
+# if sys.argv[1:]:
+#     port = int(sys.argv[1])
+# else:
+#     port = 443
+# server_address = ('', port)
+#
+# HandlerClass.protocol_version = Protocol
+# httpd = ServerClass(server_address, HandlerClass)
+#
+# sa = httpd.socket.getsockname()
+# print("Serving HTTP on", sa[0], "port", sa[1], "...")
+# httpd.serve_forever()

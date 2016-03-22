59278d9c3acc6c6ad5485841cb91ff42ed1bba4c
Jarrah-95 <jarrah-95@live.com.au>
2016-03-23 09:46:10 +1100

    DB exception handling

--- src/Server/SQL.py
+++ src/Server/SQL.py
@@ -10,22 +10,30 @@ config = {
   'use_pure': False,
 }
 
+
 def getDB():
     # Get a connection to the database.
     try:
-        con =  mysql.connector.connect(**config)
+        con = mysql.connector.connect(**config)
+
     except mysql.connector.Error as error:
         if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Login failed, user or password incorrect.\n")
         elif error.errno == errorcode.ER_BAD_DB_ERROR:
             print("Database Does not exist\n")
         else: print(error)
-    else: con.close()
-    return con
+        if con is not None:
+            con.close()
+
+    else:
+        con.close()
+    finally:
+        return con
 
 
 def runSQL(sql):
     db = getDB()
     cur = db.cursor()
     cur.execute_query(sql)
-
+    rows = cur.fetchall()
+    return rows  # This will not do what I want.
